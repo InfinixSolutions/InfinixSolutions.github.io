aafa1dea9938febcd84a2158478e3447d1f8bcc8
Serigne Ousmane Kana Ciss <kanaman6@gmail.com>
2023-11-04 18:12:59 -0500

    Prepare for GH Page Build

---
+++ build/precache-manifest.c51d2c7cc1e702f1450d5c1f8d362f54.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6c358ec6175ff554eb025ae8120ab8",
+    "url": "/Infinix/index.html"
+  },
+  {
+    "revision": "04da4f7206dbb065d44f",
+    "url": "/Infinix/static/css/2.a3632d50.chunk.css"
+  },
+  {
+    "revision": "df6c006d94ec41408b44",
+    "url": "/Infinix/static/css/main.d3919ece.chunk.css"
+  },
+  {
+    "revision": "04da4f7206dbb065d44f",
+    "url": "/Infinix/static/js/2.284bf307.chunk.js"
+  },
+  {
+    "revision": "df6c006d94ec41408b44",
+    "url": "/Infinix/static/js/main.4bac32b3.chunk.js"
+  },
+  {
+    "revision": "1aab1bef4ba74116c9d0",
+    "url": "/Infinix/static/js/runtime~main.82dae4ff.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Infinix/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Infinix/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Infinix/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Infinix/static/media/slick.f97e3bbf.svg"
+  }
+]);
