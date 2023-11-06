55af8de8e3bcdf6bdaba6e6577de4ccfff26986f
Serigne Ousmane Kana Ciss <kanaman6@gmail.com>
2023-11-05 18:17:32 -0600

    Updates

---
+++ precache-manifest.b8aa43abcf2ca693fba7a929f372b0b4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ccb0a2725d7df2885061949816d1c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60a6c6049b73af1b3bf6",
+    "url": "/static/css/2.28542a8f.chunk.css"
+  },
+  {
+    "revision": "bb2f0f3dd7505537cb19",
+    "url": "/static/css/main.d3919ece.chunk.css"
+  },
+  {
+    "revision": "60a6c6049b73af1b3bf6",
+    "url": "/static/js/2.5d6b95f9.chunk.js"
+  },
+  {
+    "revision": "bb2f0f3dd7505537cb19",
+    "url": "/static/js/main.352d468d.chunk.js"
+  },
+  {
+    "revision": "aeaf5c606a64ce9d6b4c",
+    "url": "/static/js/runtime~main.ac5a3147.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
